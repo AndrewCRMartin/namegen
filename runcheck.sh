e55fbbcc495297cf6e43b7a5e73fb38cd6857f97
Andrew Martin <Andrew C.R. Martin>
2018-10-23 13:04:06 +0100

    Updated runcheck to use abnames in /tmp

--- runcheck.sh
+++ runcheck.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 name=$1
-echo ""
-echo "Phonetics (85.5)"
-echo "================"
-./abcheckname -t 81 -p $name | sort -n -k4 | tail -5
-echo ""
-echo "Writing (80.7)"
-echo "=============="
-./abcheckname -t 76 -s $name | sort -n -k4 | tail -5
+
+abnames=/tmp/abnames.dat
+
+if [ ! -f /tmp/abnames.dat ]; then
+    echo "You must install the abnames.dat file in /tmp ($abnames)"
+else
+    echo ""
+    echo "Phonetics (85.5)"
+    echo "================"
+    ./abcheckname -n $abnames -t 81 -p $name | sort -n -k4 | tail -5
+    echo ""
+    echo "Writing (80.7)"
+    echo "=============="
+    ./abcheckname -n $abnames -t 76 -s $name | sort -n -k4 | tail -5
+fi
